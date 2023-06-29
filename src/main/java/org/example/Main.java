62881f011b48f04420f380d2aa1cc5ca199ff0d5
vladVashurin <vlad.vashurin@bk.ru>
2023-06-29 22:22:53 +0300

    Second task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.example.model.City;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,7 +18,40 @@ public class Main {
             cityList.add(new City(console.nextLine()));
         }
         console.close();
-
+        cityList = sortRegionNameASC(cityList);
         cityList.forEach(System.out::println);
+    }
+
+    private static List<City> sortNameASC(List<City> cityList) {
+        cityList.sort(new Comparator<City>() {
+            @Override
+            public int compare(City o1, City o2) {
+                return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+            }
+        });
+        return cityList;
+    }
+
+    private static List<City> sortRegionNameASC(List<City> cityList) {
+        cityList.sort(new Comparator<City>() {
+            @Override
+            public int compare(City o1, City o2) {
+                return o1.getRegion().compareTo(o2.getRegion());
+            }
+        });
+        List<City> cities = new ArrayList<>();
+        List<City> temp = new ArrayList<>();
+        String lastRegion = "";
+        for (City c : cityList) {
+            if (c.getRegion().equals(lastRegion)) {
+                temp.add(c);
+            } else {
+                temp = sortNameASC(temp);
+                cities.addAll(temp);
+                temp = new ArrayList<>();
+                temp.add(c);
+            }
+        }
+        return cities;
     }
 }
